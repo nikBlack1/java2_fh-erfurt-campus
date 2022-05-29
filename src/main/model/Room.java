263c09711b447f596eda76025bab4a648a9e7928
JannPischke <92718897+JannPischke@users.noreply.github.com>
2022-05-29 17:16:19 +0200

    person and room class added

--- src/main/model/Room.java
+++ src/main/model/Room.java
@@ -1,10 +1,33 @@
 package main.model;
-
 import main.storage.AbstractDatabaseEntity;
 
 import javax.persistence.Entity;
 import java.io.Serializable;
+
 @Entity
 public class Room extends AbstractDatabaseEntity implements Serializable{
 
+    private String RoomTitle;
+    private Integer RoomFloor;
+    private String RoomType;
+    private static Integer RoomCounter = 0;
+
+    public Room(String inRoomTitle, Integer inRoomFloor, String inRoomType){
+
+        setRoomTitle(inRoomTitle);
+        setRoomFloor(inRoomFloor);
+        setRoomType(inRoomType);
+    }
+
+    public void setRoomTitle(String inRoomTitle){
+        this.RoomTitle=inRoomTitle;
+    }
+
+    public void setRoomFloor(Integer inRoomFloor){
+        this.RoomFloor=inRoomFloor;
+    }
+
+    public void setRoomType(String inRoomType){
+        this.RoomType=inRoomType;
+    }
 }
--- src/main/model/room/Person.java
+++ src/main/model/room/Person.java
@@ -1,4 +1,48 @@
 package main.model.room;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Person {
+
+    private String AcademicTitle;
+    private String FirstName;
+    private String LastName;
+    private Integer RoomID;
+
+    public Person(String inAcademicTitle, String inFirstName, String inLastName, Integer inRoomID){
+
+        setAcademicTitle(inAcademicTitle);
+        setFirstName(inFirstName);
+        setLastName(inLastName);
+        setRoomID(inRoomID);
+    }
+
+
+    /**
+     * set functions
+     */
+    public void setAcademicTitle(String inAcademicTitle){
+        this.AcademicTitle=inAcademicTitle;
+    }
+
+    public void setFirstName(String inFistName){
+        this.FirstName=inFistName;
+    }
+
+    public void setLastName(String inLastName){
+        this.LastName=inLastName;
+    }
+
+    public void setRoomID(Integer inRoomID){
+        this.RoomID=inRoomID;
+    }
+
+    /**
+     * get functions
+     */
+
+    /**
+     * delete functions
+     */
 }
